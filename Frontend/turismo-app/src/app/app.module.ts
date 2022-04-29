ae798e429c843f3a5ad328dc14216208f9828e6b
jocsanmejia <jocsanmejiajimenez@gmail.com>
2022-04-28 20:09:54 -0500

    Agregando header, footer y pag. princiapl

--- Frontend/turismo-app/src/app/app.module.ts
+++ Frontend/turismo-app/src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ListaLugaresComponent } from './lista-lugares/lista-lugares.component';
+
+const routes: Routes = [
+
+  {path: '', redirectTo: '/app', pathMatch: 'full'},
+  {path: 'listaLugares', component: ListaLugaresComponent},
+
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ListaLugaresComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
